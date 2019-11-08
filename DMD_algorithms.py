50e2751929fd191afab9a779636c522691a8b623
nibodh <nibodh@ucsb.edu>
2019-11-08 14:34:29 -0800

    Added HankelDMD algorithm

--- DMD_algorithms.py
+++ DMD_algorithms.py
@@ -3,10 +3,11 @@
 
 # In[ ]:
 
+##### Matrix inversions using pseudo inverse, singular value based to be added
 
 import numpy as np
 
-def DMD(data,polytype,polyorder):
+def DMD(data,polytype,polyorder): #polytype and polyorder unused
     n, m, l = data.shape
 
     X = np.reshape(data[:,:-1,:],(n,(m-1)*l)) # reshaping data_p to bring all trajectories into one dimension
@@ -42,12 +43,31 @@ def eDMD(data,polytype,polyorder):
     
     return K
 
-# def HankelDMD(data):
-#     n, m, l = data.shape
+def HankelDMD(data,polytype,polyorder): #polytype and polyorder unused
+    n, m, l = data.shape   #num of time points  #num of states #num of trajectories
     
-#     m = X.shape[1] #num of time points
-#     n = X.shape[0] #num of states
-#     K = Y * np.linalg.pinv(X)
+    #Construct Hankel delay matrices per observable
+    k = 20 #guessed value for dimension of K-invariant subspace
+    p = (m-1)//k #based on size of column, number of columns is obtained
     
-#     return K
+    #Contructing Hankel matrices:
+    
+    #separating data into present and future
+    #reshaping each onbservables of trajectory into Hankel matrix per state per trajectory and transposing
+    H_X = np.transpose(np.reshape(data[:,:-1,:],(n,k,p,l)),(1,2,0,3)) #storing X data for reshaping
+    H_Y = np.transpose(np.reshape(data[:,1:,:],(n,k,p,l)),(1,2,0,3))  #storing Y data for reshaping
+    #scaling according to norm of the last column of each of the Hankel matrix
+    alpha = np.linalg.norm(H_X[:,-1,:,:],axis = 0)/np.linalg.norm(H_X[:,-1,0,0]) #calculating norm of last column of each trajectory's H_X and scaling them all with that of the first H_X
+    
+    #multiplying with scaling factors
+    for i in np.arange(0,n):
+        for j in np.arange(0,l):
+            H_X[:,:,i,j] = H_X[:,:,i,j]*alpha[i,j]
+            H_Y[:,:,i,j] = H_Y[:,:,i,j]*alpha[i,j]
+    
+    X = np.reshape(H_X,(k,p*n*l)) #unfolding Hankel matrices into dimension 1
+    Y = np.reshape(H_Y,(k,p*n*l)) #unfolding Hankel matrices into dimension 1
+    K = Y @ np.linalg.pinv(X)
+    
+    return K
 

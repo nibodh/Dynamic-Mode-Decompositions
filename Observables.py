9eedf52ccc444602aabb3b9bc72fb1f28f56cd78
nibodh <nibodh@ucsb.edu>
2019-11-13 18:23:39 -0800

    detailed comments for monomial observables

--- Observables.py
+++ Observables.py
@@ -18,10 +18,10 @@ def Monomials(data, polyorder):
     psi_data = data
     if polyorder > 1:
         psi_data = np.append(psi_data,np.ones((1,m,l)),axis = 0) #Adding 0 order polynomials and 1s
-        for i in np.arange(2,polyorder + 1):  #taking combinations possible
-            for j in combinations_with_replacement(np.arange(n),i):  #at each power, there are (p+n-1)_c_(n-1) solutions
+        for i in np.arange(2,polyorder + 1):  #taking all possible powers from 2 to polyorder
+            for j in combinations_with_replacement(np.arange(n),i):  #at each power, there are (p+n-1)_c_(n-1) solutions to the number of distinct monomials of order i. j iterates through each of them
                 prod = 1
-                for k in j: #iterating through states for combination
+                for k in j: #k iterates through the tuple that j is to increase the order of each state in the monomial
                     prod = prod*data[k,:,:] #multiplying each state for that combination
                 psi_data = np.append(psi_data,prod[np.newaxis,:,:],axis = 0)
     return psi_data
